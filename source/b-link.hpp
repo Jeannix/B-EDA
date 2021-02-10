6824e68b1640b3824c0fd5ad6c0a6ed645d245a0
Jean C <jean_16jean@hotmail.com>
2021-02-10 01:07:47 -0500

    FdFs

--- source/b-link.hpp
+++ source/b-link.hpp
@@ -1,104 +1,118 @@
-// Copyright
-#ifndef SOURCE_B_LINK_HPP_
-#define SOURCE_B_LINK_HPP_
+// Copyright OpenSource
+#ifndef SOURCE_B_enlace_HPP_
+#define SOURCE_B_enlace_HPP_
 
 #include <utility>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <iostream>
+#include <shared_mutex>
+#include <queue>
+#include <mutex>
 
-namespace EDA {
-namespace Concurrent {
+using namespace std;
 
-template <std::size_t B, typename Type>
-class BLinkTree {
-  struct Node{
-		typedef Type data_type;
-		bool hoja; 
-		data_type *clave, tam; 
-		Node** ptr; 
+namespace EDA::Concurrent {
+template<std::size_t B, typename Type>
+	class BLinkTree {
+		struct Node {
 		Node(){
-			clave=new data_type[B]; 
-			ptr=new Node*[B+1]; 
+			this->hoja=true;
+			this->enlace=nullptr;
+			//this->ptrs.resize(tam_ptrs);
 		}
-		Node(Node* &temp, Node* &Nuevo){
-			this->ptr[0]=temp;
-			this->ptr[1]=Nuevo;
-			this->hoja=false;
-			this->tam=1;
+		bool hoja{};
+		vector<Type> data;
+		Node *enlace;
+		vector<Node *> ptrs;
+		};
+		mutex mtx;
+		BLinkTree::Node* root;
+		BLinkTree::Node* nodoAncestro;
+		int tam_datos{};
+		int tam_ptrs{};
+    size_t tam_nodos{};
+
+	public:
+		typedef Type data_type;
+    shared_mutex mutex;
+		BLinkTree(){
+			this->tam_datos=B;
+			this->tam_ptrs=B+1;
+			this->tam_nodos=0;
+			this->root=nullptr;
+			nodoAncestro=nullptr;
 		}
-	}; 
-	typedef Type data_type;
-	void itebusqueda(const data_type& value, Node* &temp) const{
-		while(temp->hoja==false){
-			for(int i=0; i<temp->tam; i++){ 
-				if(value<temp->clave[i]){ 
-					temp=temp->ptr[i]; 
-					break; 
-				} 
-				if(i==temp->tam-1){ 
-					temp=temp->ptr[i+1]; 
-					break; 
-				} 
+		bool empty() const;
+            ///////////////////////////////////////
+		bool search(data_type value){
+			mtx.lock();
+			if(root==nullptr){
+				mtx.unlock();
+				return false;
 			} 
-		} 
-	}
-
-	Node* busquedaUp(Node* temp, Node* hijo){
-		Node* padre;
-		if(temp->hoja||(temp->ptr[0])->hoja){ 
-			return nullptr; 
-		} 
-		for(int i=0;i<temp->tam+1; i++){ 
-			if(temp->ptr[i]==hijo){ 
-				padre=temp; 
-				return padre; 
+			else {
+				BLinkTree::Node *dummy=root;
+				while(!dummy->hoja){
+					int iwi=upper_bound(dummy->data.begin(), dummy->data.end(), value)-dummy->data.begin();
+					dummy=dummy->ptrs[iwi];
+				}
+				int segundo_iwi=lower_bound(dummy->data.begin(), dummy->data.end(), value)-dummy->data.begin();
+				mtx.unlock();
+				return !(segundo_iwi==dummy->data.size() || dummy->data[segundo_iwi] != value);
 			}
-			else{ 
-				padre=busquedaUp(temp->ptr[i], hijo); 
-				if(padre!=nullptr) 
-					return padre; 
-			} 
 		}
-		return padre; 
-	}
- public:
-  
-  Node* raiz;
-  BLinkTree() {
-    this->raiz=nullptr;
-  }
-/*
-  ~BLinkTree() {}
-
-  std::size_t size() const {}
-*/
-  bool empty() const {
-		if(raiz==nullptr){
-			return true;
-		}
-		else{ return false; }
-	}
-  bool search(const data_type& value) const {
-		Node* temp=raiz;
-		itebusqueda(value, temp);
-			for(auto i=0;i<temp->tam;i++){ 
-				if(temp->clave[i]==value){ 
-					std::cout<<"Manin"<<std::endl;
-					return true; 
-				} 
+		void insert(data_type value){
+			if(!this->root){
+				this->root=new BLinkTree::Node;
+				//this->root->hoja=true;
+				this->root->claves.push_back(value);
+				return;
 			}
-			std::cout<<"NO"<<std::endl;
-			return false;
-  }
- 
-};//not
-/*
-  void remove(const data_type& value) {}
+			else{
+				Node* dummy=root;
+				Node* nodoAncestro=nullptr;
+				while(dummy->hoja==0){
+				}
+			}
+		}
+		void remove(data_type value);
+		bool searchParallel(data_type value, Node **&p){
+			mtx.lock();
+			ptr=&root;
+			if(root==nullptr){
+				mtx.unlock();
+				return false;
+			}
+//			queue<BLinkTree::Node*> cola;
+//			int idx;
+//			cola.push_back(root);
+//			while(!cola.empty()){
+//				int cont=cola.size();
+//				while(cont > 0){
+//					BLinkTree::Node* dummy=cola.front();
+//					idx=upper_bound(dummy->data.begin(),dummy->data.end(),value)-dummy->data.begin();
+//					cola.pop();
+//					int i=0;
+//		
+//						}
+//					}
+//				}
+//			if(ptr){
+//				BLinkTree::Node *dummy=root;
+//				while(!dummy->hoja){
+//					int iwi=upper_bound(dummy->data.begin(),dummy->data.end(),value)-dummy->data.begin();
+//					dummy=dummy->ptrs[iwi];
+//				}
+//				int segundo_iwi=lower_bound(dummy->data.begin(),dummy->data.end(),value)-dummy->data.begin();
+//				mtx.unlock();
+//				return !(segundo_iwi==dummy->data.size()||dummy->data[segundo_iwi]!=value);
+//			}
+//			return true;
+		}
 
- private:
-  data_type* data_;
-};
-*/
- 
-	 }//	 namespace Concurrent
-  }//namespace EDA
 
-#endif  // SOURCE_B_LINK_HPP_
+	};
+}
+#endif  // SOURCE_B_enlace_HPP_
